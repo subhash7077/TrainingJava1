dab802b5976476d69aae204c9601c69bcc0df267
Subhash <salke.subhash@gmail.com>
2025-06-30 10:59:08 +0530

    test

---
+++ src/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+        System.out.println("This is a test program.");
+        System.out.println("Let's see how it works.");
+    }
+}
