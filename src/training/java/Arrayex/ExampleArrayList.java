a17451c45241d37dcd3ec3045b5c3c2a5b3d1730
Subhash <salke.subhash@gmail.com>
2025-07-05 13:36:25 +0530

    sunday

--- src/training/java/Arrayex/ExampleArrayList.java
+++ src/training/java/Arrayex/ExampleArrayList.java
@@ -1,6 +1,7 @@
 package training.java.Arrayex;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class ExampleArrayList {
@@ -21,6 +22,18 @@ public class ExampleArrayList {
 
         list1.addAll(1, list2);
         System.out.println("List1 after adding List2 at index 1: " + list1);
+
+        //Iterating through the list
+
+        Iterator<Integer> itr1= list1.iterator();
+        Iterator<Integer> itr2 = list2.iterator();
+
+        while (itr1.hasNext()){
+            System.out.println("List1 element: " + itr1.next());
+        }
+        while (itr2.hasNext()){
+            System.out.println("List2 element: " + itr2.next());
+        }
 
     }
 }
